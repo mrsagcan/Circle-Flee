a6e49f4f0abd3c7dd1d421e205b1df2e9489ff82
mrsagcan <mrsagcan@yahoo.com>
2024-07-10 17:09:57 +0300

    GameManager and SoundManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private const string _mainMenu = "MainMenu";
+    private const string _gameplay = "Gameplay";
+    private string _highScoreKey = "HighScore";
+
+    public bool IsInitialized {  get; set; }
+    public int CurrentScore { get; set; }
+
+
+
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Init();
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public int HighScore
+    {
+        get
+        {
+            return PlayerPrefs.GetInt(_highScoreKey, 0);
+        }
+        set
+        {
+            PlayerPrefs.SetInt(_highScoreKey, value);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
-        
+        CurrentScore = 0;
+        IsInitialized = false;
     }
+
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_mainMenu);
+    }
+
+    public void GoToGameplay()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_gameplay);
+    }
+
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private AudioSource _effectSource;
+
+    public static SoundManager Instance;
+
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(AudioClip clip)
     {
-        
+        _effectSource.PlayOneShot(clip);
     }
 }
